ac8546eb56e7a33fa7a1a6564e18432ca3536a27
azhar0102 <azharshaik3@gmail.com>
2024-02-26 20:19:27 +0530

    integrated arithmetic operations

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 import DigitButton from "./components/DigitButton";
 import OperationButton from "./components/OperationButton";
 
@@ -27,6 +27,9 @@ const reducer = (state, { type, payload }) => {
       if (state.currentOperand == null && state.previousOperand == null) {
         return state;
       }
+      if (state.currentOperand == null) {
+        return { ...state, operation: payload.operation };
+      }
       if (state.previousOperand == null) {
         return {
           ...state,
@@ -43,19 +46,46 @@ const reducer = (state, { type, payload }) => {
       };
     case ACTIONS.CLEAR:
       return {};
+    case ACTIONS.EVALUATE:
+      if (
+        state.previousOperand == null ||
+        state.operation == null ||
+        state.currentOperand == null
+      ) {
+        return state;
+      }
+      return {
+        ...state,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state),
+      };
   }
 };
 
 const evaluate = ({ previousOperand, operation, currentOperand }) => {
   let prev = parseFloat(previousOperand);
   let curr = parseFloat(currentOperand);
-  // return eval(curr.concat(op).concat(prev));
-  return console.log(eval(curr.concat(operation).concat(prev)));
+  if (isNaN(prev) || isNaN(curr)) return "";
+  let computation = "";
+  switch (operation) {
+    case "+":
+      computation = prev + curr;
+      break;
+    case "-":
+      computation = prev - curr;
+      break;
+    case "÷":
+      computation = prev / curr;
+      break;
+    case "x":
+      computation = prev * curr;
+      break;
+  }
+  return computation.toString();
 };
 
 function App() {
-  const [number, setNumber] = useState("");
-
   const [{ previousOperand, currentOperand, operation }, dispatch] = useReducer(
     reducer,
     {}
@@ -83,11 +113,11 @@ function App() {
           <button className="red_btn">DEL</button>
           <div></div>
 
-          <OperationButton operation="&divide;" dispatch={dispatch} />
+          <OperationButton operation="÷" dispatch={dispatch} />
           <DigitButton digit="1" dispatch={dispatch} />
           <DigitButton digit="2" dispatch={dispatch} />
           <DigitButton digit="3" dispatch={dispatch} />
-          <OperationButton operation="&times;" dispatch={dispatch} />
+          <OperationButton operation="x" dispatch={dispatch} />
           <DigitButton digit="4" dispatch={dispatch} />
           <DigitButton digit="5" dispatch={dispatch} />
           <DigitButton digit="6" dispatch={dispatch} />
@@ -95,11 +125,15 @@ function App() {
           <DigitButton digit="7" dispatch={dispatch} />
           <DigitButton digit="8" dispatch={dispatch} />
           <DigitButton digit="9" dispatch={dispatch} />
-          <OperationButton operation="&ndash;" dispatch={dispatch} />
+          <OperationButton operation="-" dispatch={dispatch} />
           <DigitButton digit="0" dispatch={dispatch} />
           <DigitButton digit="." dispatch={dispatch} />
           <div></div>
-          <OperationButton operation="=" dispatch={dispatch} />
+          <OperationButton //change it to normal btn tag
+            operation="="
+            dispatch={dispatch}
+            onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
+          />
         </div>
       </div>
     </div>
